3427d3657f97c3cc68b39c33932fa504ac32c873
Matthew Wicks <mwicks7@gmail.com>
2024-04-21 12:28:16 -0400

    Update styling

--- src/Question.js
+++ src/Question.js
@@ -1,48 +1,74 @@
-import React from 'react'
+import React from "react";
 
 const AnswerForm = ({ handleAnswerQuestion, handleSkipQuestion }) => {
   return (
     <div className="question__form">
-      <input type="text" placeholder="What is..." name="answer" id="answerInput" className="question__input"/>
-      <button onClick={handleAnswerQuestion} className="question__submit">Submit Response</button>
-      <button onClick={handleSkipQuestion} className="question__submit question__submit--skip">Skip</button>
+      <input
+        type="text"
+        placeholder="What is..."
+        name="answer"
+        id="answerInput"
+        className="question__input"
+      />
+      <button onClick={handleAnswerQuestion} className="question__submit">
+        Submit Response
+      </button>
+      <button
+        onClick={handleSkipQuestion}
+        className="question__submit question__submit--skip"
+      >
+        Skip
+      </button>
     </div>
-  )
-}
+  );
+};
 
 const Answer = ({ displayMessage, answer, handleKeepPlaying }) => {
   return (
     <div className="question__result">
       <div className="question__correct">
-        {displayMessage.correct === true && 'CORRECT!'}
-        {displayMessage.correct === false && 'INCORRECT!'}
+        {displayMessage.correct === true && "CORRECT!"}
+        {displayMessage.correct === false && "INCORRECT!"}
       </div>
-      <div className="question__answer">
-        The answer is: {answer}
-      </div>
-      {displayMessage.keepPlaying && <button onClick={handleKeepPlaying} className="question__keep-playing">Keep Playing</button>}
+      <div className="question__answer">What is {answer}?</div>
+      {displayMessage.keepPlaying && (
+        <button onClick={handleKeepPlaying} className="question__keep-playing">
+          Keep Playing
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-const Question = ({ activeClue, handleAnswerQuestion, handleSkipQuestion, displayMessage, handleKeepPlaying }) => {
+const Question = ({
+  activeClue,
+  handleAnswerQuestion,
+  handleSkipQuestion,
+  displayMessage,
+  handleKeepPlaying,
+}) => {
   if (activeClue.question) {
     return (
       <div className="question">
-        <div className="question__text">
-          {activeClue.question}
-        </div>
-        {!displayMessage.answer && <AnswerForm handleAnswerQuestion={handleAnswerQuestion} handleSkipQuestion={handleSkipQuestion}/>}
-        {displayMessage.answer && <Answer displayMessage={displayMessage} answer={activeClue.answer} handleKeepPlaying={handleKeepPlaying}/>}
+        <div className="question__text">{activeClue.question}</div>
+        {!displayMessage.answer && (
+          <AnswerForm
+            handleAnswerQuestion={handleAnswerQuestion}
+            handleSkipQuestion={handleSkipQuestion}
+          />
+        )}
+        {displayMessage.answer && (
+          <Answer
+            displayMessage={displayMessage}
+            answer={activeClue.answer}
+            handleKeepPlaying={handleKeepPlaying}
+          />
+        )}
       </div>
-    )
+    );
   } else {
-    return (
-      <div className="question">
-        Select a clue
-      </div>
-    )
+    return <div className="question">Select a clue</div>;
   }
-}
+};
 
-export default Question
+export default Question;
